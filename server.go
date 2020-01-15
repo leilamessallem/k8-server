4042bc1abe263d50c5b69781ac070a0a9ceb1d64
Leila Messallem <leila.messallem@neotchnology.com>
2020-01-15 10:00:19 +0100

    Added ConfigMap and endpoint "/env" to return env vars

--- server.go
+++ server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,7 +19,8 @@ func main() {
 	router := mux.NewRouter()
 	
 	router.HandleFunc("/health", ping).Methods("GET")
-	router.HandleFunc("/add", add).Methods("POST")
+	router.HandleFunc("/add", sumTwoNumbers).Methods("POST")
+	router.HandleFunc("/env", getEnvironmentVariables).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -28,12 +31,18 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(time.Now().Format(time.RFC3339))
 }
 
-func add(w http.ResponseWriter, r *http.Request) {
+func sumTwoNumbers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var numbers Numbers
 	_ = json.NewDecoder(r.Body).Decode(&numbers)
 	result := numbers.A + numbers.B
 	json.NewEncoder(w).Encode(result)
+}
+
+func getEnvironmentVariables(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(fmt.Sprintf("%s %s", os.Getenv("LOCATION"), os.Getenv("USER")))
 }
 
